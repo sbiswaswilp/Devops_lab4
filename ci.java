98bc2dedb5e851338ba91ef26f786c9722fa25ee
sbiswaswilp <2021wa86420@wilp.bits-pilani.ac.in>
2025-07-11 17:25:46 +0530

    Modified App.java to trigger CI build

---
+++ ci.java
@@ -0,0 +1,6 @@
+public class CI {
+    public static void main(String[] args) {
+        System.out.println("Testing Jenkins CI!");
+    	System.out.println("Build triggered by CI change at " + java.time.LocalDateTime.now());
+    }
+}
